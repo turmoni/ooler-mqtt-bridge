e72e8fb34cf58c852f6ec4d4f0718b79fe7501fa
Dave Wickham <dave@dwickham.me.uk>
2022-09-25 17:11:37 +0100

    Move BLE code over to using Bleak. Still having issues with running on my Pi, but seems to work fine on my laptop near the Ooler.

--- ooler/ooler.py
+++ ooler/ooler.py
@@ -1,7 +1,7 @@
 """Monitor and control an Ooler device via Bluetooth Low Energy"""
 import logging
-import ooler.constants as constants
-from gattlib import GATTRequester, BTIOException
+from ooler import constants
+from bleak import BleakClient, BleakError
 
 
 class Ooler:
@@ -11,70 +11,51 @@ class Ooler:
         self.address = address
         self.stay_connected = stay_connected
         self.max_connection_attempts = max_connection_attempts
-        self.requester = GATTRequester(address, False)
+        self.client = None
         self.logger = logging.getLogger(__name__)
         self.logger.setLevel(logging.DEBUG)
-        if self.stay_connected:
-            self.connect()
 
-    def connect(self) -> None:
+    async def connect(self) -> None:
         """Attempt to connect to the Ooler"""
-        if self.requester.is_connected():
+        if self.client and self.client.is_connected:
             return
 
-        for attempt in range(self.max_connection_attempts):
-            try:
-                self.logger.debug(f"Attempting to connect, attempt {attempt}")
-                self.requester.connect(True)
-                self.logger.info(f"Connected to {self.address}")
-                break
-            except BTIOException as exc:
-                self.logger.warning(
-                    f"Failed to connect on attempt {attempt}, got {exc}"
-                )
-                self.requester.disconnect()
+        self.client = BleakClient(self.address)
+        self.logger.info("Attempting to connect")
+        await self.client.connect()
+        self.logger.info(f"Connected to {self.address}")
 
-        if not self.requester.is_connected():
+
+        if not self.client.is_connected:
             raise ConnectionError(
-                f"Failed to connect after {self.max_connection_attempts} goes"
+                "Failed to connect to Ooler"
             )
 
-        self.requester.exchange_mtu(512)
-        self.uuid_map = self._get_uuid_map()
-
-    def _get_uuid_map(self) -> dict:
-        """
-        Get a mapping of UUIDs to handles - this shouldn't be needed, but the
-        UUID-related functions don't seem to work for me with the Ooler
-        """
-        characteristics = self.requester.discover_characteristics()
-        return {v["uuid"]: v["value_handle"] for v in characteristics}
-
-    def _request_characteristic(self, uuid: str) -> bytes:
+    async def _request_characteristic(self, uuid: str) -> bytes:
         """Request a characteristic, handling connections and the like"""
-        if not self.requester.is_connected():
-            self.connect()
+        if not self.client.is_connected:
+            await self.connect()
 
-        value = self.requester.read_by_handle(self.uuid_map[uuid])[0]
+        value = await self.client.read_gatt_char(uuid)
 
         if not self.stay_connected:
-            self.disconnect()
+            await self.disconnect()
 
         return value
 
-    def _write_characteristic(self, uuid: str, data: bytes) -> bytes:
+    async def _write_characteristic(self, uuid: str, data: bytes) -> bytes:
         """Write a characteristic, handling connections and the like"""
-        if not self.requester.is_connected():
+        if not self.client.is_connected:
             self.connect()
 
-        self.requester.write_by_handle(self.uuid_map[uuid], data)
+        await self.client.write_gatt_char(uuid, data)
 
         if not self.stay_connected:
             self.disconnect()
 
-    def disconnect(self) -> None:
+    async def disconnect(self) -> None:
         """Disconnect from the Ooler"""
-        self.requester.disconnect()
+        await self.client.disconnect()
 
     @staticmethod
     def _f_to_c(deg_f: int) -> int:
@@ -88,99 +69,83 @@ class Ooler:
         f_float = (deg_c * 1.8) + 32
         return round(f_float)
 
-    @property
-    def actual_temperature_f(self) -> int:
+    async def get_actual_temperature_f(self) -> int:
         """Get the current tempterature in Fahrenheit"""
         return int.from_bytes(
-            self._request_characteristic(constants.ACTUAL_TEMP_F), byteorder="big"
+            await self._request_characteristic(constants.ACTUAL_TEMP_F), byteorder="big"
         )
 
-    @property
-    def actual_temperature_c(self) -> int:
+    async def get_actual_temperature_c(self) -> int:
         """Get the current tempterature in Celsius"""
-        return self._f_to_c(self.actual_temperature_f)
+        return self._f_to_c(await self.get_actual_temperature_f())
 
-    @property
-    def desired_temperature_f(self) -> int:
+    async def get_desired_temperature_f(self) -> int:
         """Get the desired tempterature in Fahrenheit"""
         return int.from_bytes(
-            self._request_characteristic(constants.TARGET_TEMP_F), byteorder="big"
+            await self._request_characteristic(constants.TARGET_TEMP_F), byteorder="big"
         )
 
-    @desired_temperature_f.setter
-    def desired_temperature_f(self, deg_f: int) -> None:
+    async def set_desired_temperature_f(self, deg_f: int) -> None:
         """Set the desired tempterature in Fahrenheit"""
-        self._write_characteristic(
+        await self._write_characteristic(
             constants.TARGET_TEMP_F, deg_f.to_bytes(1, byteorder="big")
         )
 
-    @property
-    def desired_temperature_c(self) -> int:
+    async def get_desired_temperature_c(self) -> int:
         """Get the desired tempterature in Celsius"""
-        return self._f_to_c(self.desired_temperature_f)
+        return self._f_to_c(await self.get_desired_temperature_f())
 
-    @desired_temperature_c.setter
-    def desired_temperature_c(self, deg_c: int) -> None:
+    async def set_desired_temperature_c(self, deg_c: int) -> None:
         """Set the desired tempterature in Celsius"""
-        self.desired_temperature_f = self._c_to_f(deg_c)
+        await self.set_desired_temperature_f(self._c_to_f(deg_c))
 
-    @property
-    def powered_on(self) -> bool:
+    async def powered_on(self) -> bool:
         """Return the power state of the Ooler"""
-        return self._request_characteristic(constants.POWER_STATUS) == b"\x01"
+        return await self._request_characteristic(constants.POWER_STATUS) == b"\x01"
 
-    @powered_on.setter
-    def powered_on(self, value: bool) -> None:
+    async def set_power_state(self, value: bool) -> None:
         """Turn the Ooler on or off"""
-        self._write_characteristic(
+        await self._write_characteristic(
             constants.POWER_STATUS, value.to_bytes(1, byteorder="big")
         )
 
-    @property
-    def fan_speed(self) -> constants.FanSpeed:
+    async def get_fan_speed(self) -> constants.FanSpeed:
         """Return the fan mode of the Ooler"""
-        speed = self._request_characteristic(constants.FAN_SPEED)
+        speed = await self._request_characteristic(constants.FAN_SPEED)
         return constants.FanSpeed(int.from_bytes(speed, byteorder="big"))
 
-    @fan_speed.setter
-    def fan_speed(self, speed: constants.FanSpeed) -> None:
+    async def set_fan_speed(self, speed: constants.FanSpeed) -> None:
         """Return the fan mode of the Ooler"""
-        self._write_characteristic(
+        await self._write_characteristic(
             constants.FAN_SPEED, speed.value.to_bytes(1, byteorder="big")
         )
 
-    @property
-    def water_level(self) -> int:
+    async def get_water_level(self) -> int:
         """Return the water level of the Ooler"""
         return int.from_bytes(
-            self._request_characteristic(constants.WATER_LEVEL), byteorder="big"
+            await self._request_characteristic(constants.WATER_LEVEL), byteorder="big"
         )
 
-    @property
-    def pump_wattage(self) -> int:
+    async def get_pump_wattage(self) -> int:
         """Return the wattage of the pump"""
         return int.from_bytes(
-            self._request_characteristic(constants.PUMP_WATTS), byteorder="big"
+            await self._request_characteristic(constants.PUMP_WATTS), byteorder="big"
         )
 
-    @property
-    def pump_voltage(self) -> int:
+    async def get_pump_voltage(self) -> int:
         """Return the volttage of the pump"""
         return int.from_bytes(
-            self._request_characteristic(constants.PUMP_VOLTS), byteorder="big"
+            await self._request_characteristic(constants.PUMP_VOLTS), byteorder="big"
         )
 
-    @property
-    def cleaning(self) -> bool:
+    async def is_cleaning(self) -> bool:
         """Return whether the device is cleaning itself"""
-        return self._request_characteristic(constants.CLEAN) == b"\x01"
+        return await self._request_characteristic(constants.CLEAN) == b"\x01"
 
-    @cleaning.setter
-    def cleaning(self, value: bool) -> None:
+    async def set_cleaning(self, value: bool) -> None:
         """Tell the device to clean"""
-        self._write_characteristic(constants.CLEAN, value.to_bytes(1, byteorder="big"))
+        await self._write_characteristic(constants.CLEAN, value.to_bytes(1, byteorder="big"))
 
-    @property
-    def name(self) -> str:
+    async def get_name(self) -> str:
         """Get the name of the Ooler"""
-        return self._request_characteristic(constants.NAME).decode(encoding="ascii")
+        return (await self._request_characteristic(constants.NAME)).decode(encoding="ascii")
